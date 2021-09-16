bfb62d280079b7e28eb3856008e3aa6cefadbaea
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-09-16 19:46:51 -0400

    Update streamlit_app.py

--- folder_1/streamlit_app.py
+++ folder_1/streamlit_app.py
@@ -7,9 +7,9 @@ p = pathlib.Path(".")  #current folder (scripts)
 # st.write("Current folder:")
 # st.write(p)
 parent = p.resolve().parent.resolve() #parent folder 
-libfolder = parent / "scmopt"  
-new_path = sys.path.append(str(libfolder)) # add both paths 
-new_path_1 = sys.path.append(str(parent))
+# libfolder = parent / "scmopt"  
+# new_path = sys.path.append(str(libfolder)) # add both paths 
+new_path_1 = sys.path.append(../..)
 st.write(new_path_1)
 # st.write(parent)
 # st.write(libfolder)
