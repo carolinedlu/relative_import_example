284e5099afe8d4f314b6d0fad8e25775833c9f0e
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-09-16 19:22:38 -0400

    Update streamlit_app.py

--- folder_1/streamlit_app.py
+++ folder_1/streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pathlib
 
 current_path = pathlib.Path(__file__).parent.resolve()
+
+st.write("Path to Python file containing Streamlit app (using pathlib):")
 st.write(current_path)
